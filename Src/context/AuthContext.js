b9aca3ad12309abfd9f9d4efa2b047f86709760a
michael-DDOG <152128311+michael-DDOG@users.noreply.github.com>
2025-08-08 00:42:53 -0400

    Update AuthContext.js

--- Src/context/AuthContext.js
+++ Src/context/AuthContext.js
@@ -1,1 +1,11 @@
+// src/context/AuthContext.js
+import { createContext } from 'react';
 
+const AuthContext = createContext({
+  signIn: async () => {},
+  signOut: async () => {},
+  userData: null,
+  userToken: null
+});
+
+export default AuthContext;
