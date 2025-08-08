7d9f710e9af952ba77a4719273918b65e69c1277
michael-DDOG <152128311+michael-DDOG@users.noreply.github.com>
2025-08-08 00:41:16 -0400

    Update config.js

--- Src/i18n/config.js
+++ Src/i18n/config.js
@@ -1,1 +1,21 @@
+// src/i18n/config.js
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './translations/en.json';
+import ur from './translations/ur.json';
 
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      ur: { translation: ur }
+    },
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
